2a3cdbb49d368b7e99138fbed0901a153dbefcd7
VladBermishev <vlad@mdo.ru>
2025-05-06 18:28:38 +0300

    - Added mempool to prevent memory leaking - Refactored basic_std library

--- std-library/main.cpp
+++ std-library/main.cpp
@@ -1,37 +1,67 @@
 #include <cstdio>
-#include <cstring>
-#include <cstdlib>
-#include <cwchar>
 #include <clocale>
-extern "C" void PrintI(int val){ printf("%d", val); }
-extern "C" void PrintL(long val){ printf("%ld", val); }
-extern "C" void PrintD(double val){ printf("%lf", val); }
-extern "C" void PrintF(float val){ printf("%f", val); }
-extern "C" void PrintS(wchar_t* val){ printf("%ls", val); }
-extern "C" wchar_t* StringConcat(wchar_t* lhs, wchar_t* rhs){
-    unsigned long __sz_lhs = wcslen(lhs), __sz_rhs = wcslen(rhs);
-    lhs = (wchar_t*)reallocarray(lhs, __sz_lhs + __sz_rhs + 1, sizeof(wchar_t));
-    return wcscat(lhs, rhs);
+#include <codecvt>
+#include <vector>
+#include <string>
+#include <cuchar>
+#include <forward_list>
+#include <list>
+#include <ranges>
+#include <format>
+
+namespace basic_std{
+    using string_type = std::u32string;
+    using __abi_args_type = string_type::traits_type::char_type**;
+    std::forward_list<string_type> pool;
+
+    std::vector<string_type::pointer> convert_args(std::list<string_type>& args){
+        std::vector<string_type::pointer> result(args.size(), nullptr);
+        for(auto [result_value, value] : std::views::zip(result, args))
+            result_value = value.data();
+        return std::move(result);
+    }
 }
-extern "C" wchar_t* StringCopy(wchar_t* str){
-    unsigned long __sz = wcslen(str);
-    wchar_t* result = (wchar_t*)calloc(__sz + 1, sizeof(wchar_t));
-    wcsncpy(result, str, __sz);
-    return result;
+
+extern "C"{
+    void PrintI(int val){ printf("%d", val); }
+    void PrintL(long val){ printf("%ld", val); }
+    void PrintD(double val){ printf("%lf", val); }
+    void PrintF(float val){ printf("%f", val); }
+    void PrintS(basic_std::string_type::pointer val){ printf("%ls", val); }
+
+    basic_std::string_type::pointer StringConcat(basic_std::string_type::pointer lhs, basic_std::string_type::pointer rhs){
+        std::size_t __lhs_length = basic_std::string_type::traits_type::length(lhs);
+        std::size_t __rhs__length = basic_std::string_type::traits_type::length(rhs);
+        basic_std::pool.emplace_front(__lhs_length + __rhs__length + 1, '\0');
+        basic_std::string_type::traits_type::copy(basic_std::pool.front().data(), lhs, __lhs_length);
+        basic_std::string_type::traits_type::copy(std::next(basic_std::pool.front().data(), __lhs_length), rhs, __rhs__length);
+        return basic_std::pool.front().data();
+    }
+
+    basic_std::string_type::pointer StringCopy(basic_std::string_type::pointer str){
+        std::size_t __sz = basic_std::string_type::traits_type::length(str);
+        basic_std::pool.emplace_front(__sz + 1, '\0');
+        basic_std::string_type::traits_type::copy(basic_std::pool.front().data(), str, __sz);
+        return basic_std::pool.front().data();
+    }
+
+    void StringFree(basic_std::string_type::pointer str){
+        const auto&& __pred = [str](const basic_std::string_type& value){ return value.data() == str; };
+        std::size_t __removed = basic_std::pool.remove_if(__pred);
+        if(__removed > 1)
+            throw std::runtime_error(std::format("StringFree bad free: removed {} identical items", __removed));
+    }
+
+    void Main(basic_std::__abi_args_type argv, int len);
 }
-extern "C" wchar_t* Main(wchar_t** argv, int len);
+
 int main(int argc, char** argv){
     setlocale(LC_ALL, "C.UTF-8");
-    wchar_t** args = (wchar_t**)calloc(argc, sizeof(wchar_t*));
-    for(int idx = 0; idx < argc; idx++){
-        unsigned long __sz = strlen(argv[idx]);
-        args[idx] = (wchar_t*) calloc(__sz + 1,sizeof(wchar_t));
-        for(int row_idx = 0; row_idx < __sz; row_idx++)
-            args[idx][row_idx] = argv[idx][row_idx];
-    }
-    Main(args, argc);
+    std::wstring_convert<std::codecvt_utf8<char32_t>, char32_t> converter{};
+    std::list<std::u32string> args;
     for(int idx = 0; idx < argc; idx++)
-        free(args[idx]);
-    free(args);
+        args.push_back(converter.from_bytes(argv[idx]));
+    auto native_args = basic_std::convert_args(args);
+    Main(native_args.data(), argc);
     return 0;
 }
